c390cb50d685bb433198b08daa00b37e862b1033
Brook Riggio <brooke.riggio@gmail.com>
2013-10-06 00:56:05 -0700

    Now that's what I call fresh...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '0d759491461444e3973b9a749eae6b60be2e80592a267a43bedc39f238f5717c202a326750b0941d7b947fb90e313e7605c8235fb5abcbe9114be6435a9264f4'
